ce7ab76ec59992d24547b7dfb99be1819d68a551
TheNecker <chrisp132012@gmail.com>
2017-08-15 21:23:26 -0400

    Add files via upload

---
+++ Subtract.rb
@@ -0,0 +1,11 @@
+def Subtraction(num_1,num_2)
+
+puts "#{num_1}-#{num_2} = #{num_1 - num_2}"
+end 
+puts "What is your first number?"
+num_1 = gets.chomp.to_i
+puts "Ok, and your second number?"
+num_2 = gets.chomp.to_i
+
+
+Subtraction(num_1,num_2) 
